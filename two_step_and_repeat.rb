4c734e40435910c16a07881c0d5f854dd0879011
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-05 06:59:08 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -2,6 +2,12 @@ def first_steps
   loop do
   puts "Right foot back"
   sleep(0.5)
+  puts "Left foot back"
+  sleep(0.5)
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Stop"
+  sleep(1)
 end
 end
 
